2f03991f8a84b1f1e14f64dd21b3c6d68486d7e1
JustDimus <57843014+JustDimus@users.noreply.github.com>
2021-05-06 15:36:13 +0300

    Add Irepository Interface

---
+++ Boarsenger.API/Boarsenger.API.DAL/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boarsenger.API.DAL.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+    }
+}
