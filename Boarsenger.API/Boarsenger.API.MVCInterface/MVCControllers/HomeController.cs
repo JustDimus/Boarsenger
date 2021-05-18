92a503060d4de0848938867824dac91f86e3752c
JustDimus <57843014+JustDimus@users.noreply.github.com>
2021-05-18 10:20:20 +0300

    Created APIControllers Folder

--- Boarsenger.API/Boarsenger.API.MVCInterface/MVCControllers/HomeController.cs
+++ Boarsenger.API/Boarsenger.API.MVCInterface/MVCControllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Boarsenger.API.MVCInterface.Controllers
+namespace Boarsenger.API.MVCInterface.MVCControllers
 {
     public class HomeController : Controller
     {
