066f2e90ddbc78f30c7c13947f41b6249375f063
JustDimus <57843014+JustDimus@users.noreply.github.com>
2021-05-18 11:48:35 +0300

    Created APIAccountController

---
+++ Boarsenger.API/Boarsenger.API.MVCInterface/APIControllers/APIAccountController.cs
@@ -0,0 +1,21 @@
+﻿using Boarsenger.API.MVCInterface.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boarsenger.API.MVCInterface.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIAccountController : ControllerBase
+    {
+
+        public async Task<IActionResult> Authorize(LoginViewModel loginModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
