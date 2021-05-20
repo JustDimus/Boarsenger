244ff60c6182e7d8be385e692bfab87a6c1d6472
JustDimus <57843014+JustDimus@users.noreply.github.com>
2021-05-20 14:50:52 +0300

    Created Services

--- Boarsenger.API/Boarsenger.API.BLL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Boarsenger.API.BLL
-{
-    public class Class1
-    {
-    }
-}
---
+++ Boarsenger.API/Boarsenger.API.BLL/Service/Implementations/ServiceResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boarsenger.API.BLL.Service.Implementations
+{
+    public class ServiceResult : IServiceResult
+    {
+        public bool IsSuccesful { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public class ServiceResult<TResult> : ServiceResult, IServiceResult<TResult>
+    {
+        public TResult Result { get; set; }
+    }
+}
