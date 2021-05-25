b5d3612b1a6c665205137ed547f19dd43b1324bd
JustDimus <57843014+JustDimus@users.noreply.github.com>
2021-05-25 15:26:57 +0300

    Created MVCAuthorization

---
+++ Boarsenger.API/Boarsenger.API.BLL/Service/IAuthorizationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boarsenger.API.BLL.Service
+{
+    public interface IAuthorizationService
+    {
+        string GetAccountToken();
+    }
+}
--- Boarsenger.API/Boarsenger.API.MVCInterface/MVCControllers/AccountController.cs
+++ Boarsenger.API/Boarsenger.API.MVCInterface/MVCControllers/AccountController.cs
@@ -1,13 +1,17 @@
 ﻿using Boarsenger.API.MVCInterface.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Boarsenger.API.MVCInterface.MVCControllers
 {
+    [Authorize]
     [Route("[controller]/[action]")]
     public class AccountController : Controller
     {
@@ -17,24 +21,28 @@ namespace Boarsenger.API.MVCInterface.MVCControllers
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public async Task<IActionResult> Login([FromForm]LoginViewModel loginModel)
         {
             return View();
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public async Task<IActionResult> Registration([FromForm]RegisterViewModel registrationModel)
         {
             return View();
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult Registration()
         {
             return View();
@@ -48,6 +56,28 @@ namespace Boarsenger.API.MVCInterface.MVCControllers
         public IActionResult ProfileEdit()
         {
             return View("");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogOut()
+        {
+            await this.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return this.RedirectToAction("Index", "Home");
+        }
+
+        private async Task AuthenticateAsync(string accountToken)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.SerialNumber, accountToken)
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await this.HttpContext.SignInAsync(principal);
         }
     }
 }
